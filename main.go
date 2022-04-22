861803db582e79cbfa363d46473584428a8120cf
Peter <petr.predtechensky@gmail.com>
2022-04-22 11:36:51 +0300

    Fix eviction policy: add debug output

--- main.go
+++ main.go
@@ -524,6 +524,7 @@ func main() {
 							log.Infof("Pod for eviction: %s, in namespace: %s", podName, p.Namespace)
 
 							evictionPolicy := policyv1beta1.Eviction{metav1.TypeMeta{APIVersion: "policy/v1beta1", Kind: "Eviction"}, metav1.ObjectMeta{Name: podName, Namespace: p.Namespace}, &metav1.DeleteOptions{}}
+							log.Infof("%v", evictionPolicy)
 
 							err := clientset.CoreV1().Pods("").EvictV1beta1(context.TODO(), &evictionPolicy)
 							if err != nil {
